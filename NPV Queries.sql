2f929981073325c17d73df275164bd3c79e7dbbe
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-04-23 16:57:17 -0400

    NPV Queries
    1421. NPV Queries from Leetcode

---
+++ NPV Queries.sql
@@ -0,0 +1,54 @@
+Write an SQL query to find the npv of all each query of queries table.
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+NPV table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2018   | 100    |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 11   | 2020   | 99     |
+| 7    | 2019   | 0      |
++------+--------+--------+
+
+Queries table:
++------+--------+
+| id   | year   |
++------+--------+
+| 1    | 2019   |
+| 2    | 2008   |
+| 3    | 2009   |
+| 7    | 2018   |
+| 7    | 2019   |
+| 7    | 2020   |
+| 13   | 2019   |
++------+--------+
+
+Result table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 7    | 2018   | 0      |
+| 7    | 2019   | 0      |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
++------+--------+--------+
+
+
+select
+    q.id
+    ,q.year
+    ,ifnull(np.npv,0) npv
+from Queries q
+left join NPV np on np.id=q.id
+and np.year=q.year
